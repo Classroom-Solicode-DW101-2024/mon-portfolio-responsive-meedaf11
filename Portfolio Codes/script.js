f3d30b003ac9e71c8fa59fb49244236b815ecc56
meedaf11 <meedaf11@gmail.com>
2025-02-02 15:53:52 +0100

    Finish Portfolio Project

--- Portfolio Codes/script.js
+++ Portfolio Codes/script.js
@@ -1,3 +1,14 @@
+window.addEventListener("scroll", function () {
+    let header = document.getElementById("header");
+
+    if (window.scrollY > 50) { 
+        header.classList.add("scrolled"); 
+    } else {
+        header.classList.remove("scrolled"); 
+    }
+});
+
+
 document.getElementById('menuToggle').addEventListener('click' , () =>{
 
     const menu = document.querySelector('.navList');
@@ -13,3 +24,40 @@ document.getElementById('menuToggle').addEventListener('click' , () =>{
     }
 
 })
+
+document.querySelector(".clickable-div").addEventListener("click", function() {
+    document.getElementById("particles-js").scrollIntoView({ behavior: "smooth" });
+});
+
+const sections = document.querySelectorAll(".section");
+const navLinks = document.querySelectorAll(".navList li");
+
+function updateActiveNav() {
+    let currentSection = "";
+
+    sections.forEach((section) => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+
+        if (window.scrollY >= sectionTop - sectionHeight / 3) {
+            currentSection = section.getAttribute("id");
+        }
+    });
+    navLinks.forEach((li) => {
+        li.classList.remove("Active");
+
+        const link = li.querySelector("a");
+        if (link.getAttribute("href").substring(1) === currentSection) {
+            li.classList.add("Active");
+        }
+    });
+
+    if (window.scrollY < 50) {
+        navLinks.forEach(li =>{
+            li.classList.remove("Active");
+        })
+        document.querySelector(".navHome").classList.add("Active");
+    }
+}
+
+window.addEventListener("scroll", updateActiveNav);
